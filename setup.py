ecd2353f4f4c1f98fca75e047fd42c33a2781116
aditisrao29-a11y <aditisrao29@gmail.com>
2026-06-24 14:00:22 +0530

    changes in setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,17 @@
-from setuptools import find_packages,setup
-from typing import List
-hypen="-e ."
-def get_req(file_path:str)->List[str]:
-    req=[]
-    with open(file_path) as obj:
-        req=obj.readlines()
-        req=[i.replace("\n"," ")for i in req]
-        if hypen in req:
-            req.remove(hypen)
-    return req        
+from setuptools import find_packages, setup
 
+def get_requirements(file_path):
+    with open(file_path) as file:
+        requirements = file.read().splitlines()
+
+        if "-e ." in requirements:
+            requirements.remove("-e .")
+
+    return requirements
 
 setup(
     name="Mlproject",
     version="0.0.1",
     author="Aditi Rao",
     packages=find_packages(),
-    install_requires=get_req('requirements.txt')
-
-)
+    install_requires=get_requirements("requirements.txt"))
