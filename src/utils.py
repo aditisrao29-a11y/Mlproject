f62c5eb78fd2557c8501060215e094b27f85b184
aditisrao29-a11y <aditisrao29@gmail.com>
2026-06-26 11:04:15 +0530

    app.py done

--- src/utils.py
+++ src/utils.py
@@ -29,6 +29,11 @@ def evaluate_model(xtrain,ytrain,xtest,ytest,models):
             return report
     except Exception as e:
         raise CustomException(e,sys)
-    
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
 
 
